1b015eb29d431aac20496cafd93b100f612874be
Navin Yadav <naviny058@gmail.com>
2025-12-05 07:53:01 +0530

    added flights model with foreign key constraints define during models defination

--- src/models/airplane.js
+++ src/models/airplane.js
@@ -11,6 +11,19 @@ module.exports = (sequelize, DataTypes) => {
      */
     static associate(models) {
       // define association here
+      this.hasMany(models.Flight, {
+        foreignKey: 'airplaneId',
+        onDelete: 'CASCADE'
+      })
+      this.hasMany(models.Flight, {
+        foreignKey: 'departureAirportId',
+        onDelete: 'CASCADE'
+      })
+      this.hasMany(models.Flight, {
+        foreignKey: 'arrivalAirportId',
+        onDelete: 'CASCADE'
+      })
+
     }
   }
   Airplane.init({
